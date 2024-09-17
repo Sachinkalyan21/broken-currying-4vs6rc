d58a84e298a5ac86816546a536d01f91e42baa87
Sachinkalyan21 <sachinkalyan2001@gmail.com>
2024-09-17 15:51:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App amma">
+      <h1>Habit Tracker</h1>
+      <h2>The habit the user wants to track</h2>
+      <input></input>
+      <button>search</button>
+      <div className="sachin">
+        <li>Exercise</li>
+        <li>Meditation</li>
+      </div>
+    </div>
+  );
+}
